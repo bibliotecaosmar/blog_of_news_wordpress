13b33b22bc12b7d763c161a993766c6d3f3646dd
Osmar Pereira <bibliotecaosmar@gmail.com>
2019-07-19 13:42:41 -0300

    Fix anything.

--- wp_template/wp-content/themes/btwp/functions.php
+++ wp_template/wp-content/themes/btwp/functions.php
@@ -7,13 +7,11 @@ function btwp_title_tag() {
 add_action('after_setup_theme', 'btwp_title_tag');
 
 // Previnir o erro na tag title em versões antigas
-if (!function('_wp_render_title_tag')) {
+/*if (!function('_wp_render_title_tag')) {
   function btwp_render_title() {
     ?>
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php
   }
   add_action('wp_head', 'btwp_render_title');
-}
-
-?>
+}*/
--- wp_template/wp-content/themes/btwp/index.php
+++ wp_template/wp-content/themes/btwp/index.php
@@ -62,7 +62,7 @@
       <div class="row">
 
         <div class="col-md-8 col-sm-12">
-          <?php if(have_posts() : while(have_posts()) : the_post(); ?>
+          <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
           <div class="blog-post">
             <h3 class="mb-3 pb-2 border-bottom"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -81,7 +81,7 @@
 
           <?php endwhile; ?>
 
-        <?php else : get_404_template(); endif; ?>
+          <?php else : get_404_template(); endif; ?>
           <div class="col-md-12 col-lg-6 mb-3">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde eum repellat, facere, ipsa dolor a pariatur quod voluptatem soluta maiores ea labore, vel quasi facilis aut? Rem consequatur architecto ea quasi qui ullam quaerat sit nostrum, necessitatibus error laboriosam? Corporis eos mollitia, reprehenderit nesciunt vitae magni ducimus voluptatem rem cumque.</p>
           </div>
